489e33678fb3957deb9d8532997e03184c92551b
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-08-24 00:46:39 +0300

    add Canvas and 3d model pc, create process loader, show 3d model for L monitor and mobile

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { Html, useProgress } from '@react-three/drei'
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className='canvas-load'></span>
+      <p className='font-bold text-[14px] mt-2'>{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const { scene } = useGLTF('/desktop_pc/scene.gltf'); // Исправлен путь к файлу
+
   return (
-    <div>Computers</div>
-  )
-}
+    <>
+      <hemisphereLight intensity={1} groundColor={'black'} />
+      <pointLight intensity={2} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={scene}
+        scale={isMobile ? .5 : .75}
+        position={isMobile ? [0, -3, -1] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.02, -0.1]}
+      />
 
-export default Computers
+    </>
+  );
+};
+
+const ComputerCanvas = () => {
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setMobile(mediaQuery.matches);
+
+    const handleMediaQuearyChange = (event) => {
+      setMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQuearyChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQuearyChange);
+    }
+  }, [])
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
