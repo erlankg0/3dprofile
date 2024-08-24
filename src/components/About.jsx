2076798bd0126ec1ef84437862b214fe91ca0c51
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-08-24 20:47:00 +0300

    about section, create HOC SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import { Tilt } from 'react-tilt';
-import { motion} from 'framer-motion';
+import { motion } from 'framer-motion';
+import { SectionWrapper } from '../hoc';
 
+import { styles } from "../style"
+import { services } from "../contstans";
+import { fadeIn, textVariant } from "../utils/motion"
 
-import {styles} from "../style"
-import {services} from "../contstans";
-import {fadeIn, textVariant} from "../utils/motion"
-
-const ServiceCard = ({index, title, icon}) =>{
+const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-       variants={fadeIn('right', 'spring', 0.5 * index, 1)}
-       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        variants={fadeIn('right', 'spring', 0.5 * index, 1)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
-          <div
-            options={{
-              max: 45,
-              scale: 1,
-              speed: 450
-            }}
-            className='bg-tertiary rounded-[20px] py-5 px-12'
-          >
-            {title}
-            <img src={icon}/>
-          </div>
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex  justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
       </motion.div>
     </Tilt>
   )
@@ -39,22 +39,22 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('','', .1, 1)}
+        variants={fadeIn('', '', .1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm skilled software developer with experince in TypeScript nad JavaScript, and expertise in framerowks like, React and Three.js. 
+        I'm skilled software developer with experince in TypeScript nad JavaScript, and expertise in framerowks like, React and Three.js.
         I'm quick learner and collabare closely with clients to create efficient, scalable and user-friendly solutions that solve real-world problem,
         Let's work togother to brint your idieals to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index)=> (
+        {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
-        ))} 
+        ))}
 
       </div>
     </>
   )
 }
 
-export default About
+export default SectionWrapper(About, 'about');
