2dc0356ad07ccd014aabd98b30a98095a3608e3a
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-08-24 08:25:20 +0300

    add tilt start to  about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion} from 'framer-motion';
+
+
+import {styles} from "../style"
+import {services} from "../contstans";
+import {fadeIn, textVariant} from "../utils/motion"
+
+const ServiceCard = ({index, title, icon}) =>{
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+       variants={fadeIn('right', 'spring', 0.5 * index, 1)}
+       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+          <div
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }}
+            className='bg-tertiary rounded-[20px] py-5 px-12'
+          >
+            {title}
+            <img src={icon}/>
+          </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.heroHeadText}>Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('','', .1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm skilled software developer with experince in TypeScript nad JavaScript, and expertise in framerowks like, React and Three.js. 
+        I'm quick learner and collabare closely with clients to create efficient, scalable and user-friendly solutions that solve real-world problem,
+        Let's work togother to brint your idieals to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index)=> (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))} 
+
+      </div>
+    </>
   )
 }
 
